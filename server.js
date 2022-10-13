6403efb3af5a71cf72330bab3dc7c22f84caf460
bouyahiali <a_bouyahi@yahoo.fr>
2022-10-13 16:52:36 +0100

    Update-Node-checkpoint

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const http = require('http');
 const server = http.createServer( (req,res) => {
     res.end('<h1>Hello Node!!!!</h1>')
 })
-server.listen(3000, 'localhost');
-console.log('Server running at http://localhost:3000');
+server.listen(3000,  () => {
+  console.log(`Server is running on PORT: 3000 );
+});
